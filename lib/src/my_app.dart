14b67706862de5d889824d0ce58e63e26f7cafed
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-05-18 11:21:58 +0700

    :pencil: setup localization

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,5 +1,7 @@
 import 'package:app_language/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -8,6 +10,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App Language',
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('in', ''),
+      ],
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
